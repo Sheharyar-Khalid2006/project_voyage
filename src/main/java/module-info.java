0a1bac40203f9414bbab1415890780df01e4000a
Sheharyar-Khalid2006 <mohmmadsheheryar006@gmail.com>
2026-04-09 20:17:36 +0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.oop.voyage.project_voyage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.oop.voyage.project_voyage to javafx.fxml;
+    exports com.oop.voyage.project_voyage;
+}
