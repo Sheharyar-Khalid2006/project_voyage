167e4076d2d5e174aa638386f25062b0aedfd226
faizanasif297 <faizanasif297@gmail.com>
2026-04-25 14:12:40 +0500

    Added the new packages for java modules to be able to access and use

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.oop.voyage.project_voyage {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -10,5 +9,12 @@ module com.oop.voyage.project_voyage {
     requires eu.hansolo.tilesfx;
 
     opens com.oop.voyage.project_voyage to javafx.fxml;
+    opens com.oop.voyage.project_voyage.model to javafx.fxml;
+    opens com.oop.voyage.project_voyage.services to javafx.fxml;
+    opens com.oop.voyage.project_voyage.interfaces to javafx.fxml;
+
     exports com.oop.voyage.project_voyage;
+    exports com.oop.voyage.project_voyage.model;
+    exports com.oop.voyage.project_voyage.services;
+    exports com.oop.voyage.project_voyage.interfaces;
 }
